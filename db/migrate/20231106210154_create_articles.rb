2a4f8a71c839585d84aff798386476e26b18f59d
Kenyon Nyangai <kenyonnyangai@Kenyons-Air.hsd1.mn.comcast.net>
2023-11-08 07:33:16 -0600

    add model categories

--- db/migrate/20231106210154_create_articles.rb
+++ db/migrate/20231106210154_create_articles.rb
@@ -3,8 +3,9 @@ class CreateArticles < ActiveRecord::Migration[7.1]
     create_table :articles do |t|
       t.string :title
       t.text :body
+      t.references :Categories, null: false, foreign_key: true
 
-      t.timestamps #create two column (created at and updated at)
+      t.timestamps # create two column (created at and updated at)
     end
   end
 end
