52cf41247426b83e6f47859ef3ea088c2e2c871d
Kenyon Nyangai <kenyonnyangai@Kenyons-MacBook-Air.local>
2023-12-20 11:17:13 -0600

    added fixtures and tests for article controller

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @category = categories(:one)
+    @article = articles(:one) #Fixtures create dummy data you can use to test your controller (you can use erb code in fixtures)
+  end
+
+  test "should get index" do
+    get category_article_path(@category, @article)
+    assert_response :success
+  end
+
+  test "should show article" do
+    get category_article_path
+  end
+
+  test "should get new" do
+    sign_in @user
+    get new_category_article_url(@category)
+    assert_response :success
+  end
+
+  test "should update article" do
+    sign_in @user
+
+  end
 end
